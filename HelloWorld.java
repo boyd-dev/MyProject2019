09e8d94fb2b6176e5a9952d8804c3457f4383ace
boyd-dev <poorpixel@gmail.com>
2019-01-22 07:00:49 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.song.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Git!");
+		System.out.println("Hello GitHub!");
+
+	}
+
+}
