02c584c1962654873ed8f345c22656b2333644dc
codefooo <codefooo@foo.com>
2020-02-11 19:25:30 +0900

    .

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 			System.out.println("Hello Git!");
 			System.out.println("Hello GitHub!!!");
 			System.out.println("Hello World!");
-			
+			System.out.println("Hello MyProject2019");
 
 	}
 
